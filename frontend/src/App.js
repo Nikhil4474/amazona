8c5ffdb96781a15e6606ffc086012fdcf6d18a0c
Nikhil4474 <ninuganti15@gmail.com>
2022-07-13 14:15:06 -0700

    Adding styling to images and added button

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,14 +7,25 @@ function App() {
         <a href="/">Amazona</a>
       </header>
       <main>
-        <h1>Fetured Products</h1>
-        {data.products.map((product) => (
-          <div>
-            <img src={product.image} alt={product.name} />
-            <p>{product.name}</p>
-            <p>${product.price}</p>
-          </div>
-        ))}
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))}
+        </div>
       </main>
     </div>
   );
