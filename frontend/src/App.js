678e3a1fe84a1c445c94878738c644d75d479862
Nikhil4474 <ninuganti15@gmail.com>
2022-07-19 22:52:24 -0700

    Changed App Name

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -31,7 +31,7 @@ function App() {
           <Navbar bg="dark" variant="dark">
             <Container>
               <LinkContainer to="/">
-                <Navbar.Brand>amazona</Navbar.Brand>
+                <Navbar.Brand>Amazona</Navbar.Brand>
               </LinkContainer>
               <Nav className="me-auto">
                 <Link to="/cart" className="nav-link">
